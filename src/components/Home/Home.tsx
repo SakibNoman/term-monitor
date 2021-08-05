b5d35dc8f57092c221046bde3a43b678dea9baee
Sakib Noman <skbnmn@gmail.com>
2021-08-06 00:24:29 +0600

    side bar info

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="layout">
+      <div className="sideBar"></div>
+      <div className="mainBody"></div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/SideBar/SideBar.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const sideBarInfo = [
+  {
+    id: 1,
+    icon: "",
+    title: "Add keywords",
+  },
+  {
+    id: 2,
+    icon: "",
+    title: "Matches",
+  },
+  {
+    id: 3,
+    icon: "",
+    title: "Manage sources",
+  },
+  {
+    id: 4,
+    icon: "",
+    title: "Integration",
+  },
+  {
+    id: 5,
+    icon: "",
+    title: "Alerts",
+  },
+];
+
+const SideBar = () => {
+  return <></>;
+};
+
+export default SideBar;
